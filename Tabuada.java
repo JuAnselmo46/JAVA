4724fe06fb48523bdadf1d07b7912de19987f5b5
JuAnselmo46 <juliana.anselmo1a@gmail.com>
2025-03-31 13:35:57 -0300

    Ex de tabuada- Desenvolver um programa de tabuada.

---
+++ Tabuada.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Tabuada- Desenvolver um programa de tabuada.
+
+*******************************************************************************/
+import java.util.Scanner; 
+public class Tabuada
+{
+	public static void main(String[] args) {
+	    Scanner ler = new Scanner(System.in);
+	    int t, resultado;
+		
+		System.out.println("Entre com a tabuada: ");
+		t= ler.nextInt();
+		
+		for(int i = 1; i <=10;i++){
+		    resultado = t*i;
+		    System.out.println(t + "X" + i + "=" + resultado );
+		}
+	}
+}
