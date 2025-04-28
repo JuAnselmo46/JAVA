55729ead5c51ac67dc74ca267426b20a0931af77
JuAnselmo46 <juliana.anselmo1a@gmail.com>
2025-04-28 15:15:36 -0300

    Ex57 Crie um programa que solicite ao usuário um número e exiba a tabuada desse número utilizando um laço de repetição. Utilize while.

---
+++ Ex57.java
@@ -0,0 +1,22 @@
+/****************************************************************************
+Crie um programa que solicite ao usuário um número e exiba a tabuada desse número utilizando um laço de repetição. Utilize while.
+*******************************************************************************/
+
+import java.util.Scanner;
+
+public class Ex57 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número para exibir sua tabuada: ");
+        int numero = scanner.nextInt();
+
+        int i = 1;
+        System.out.println("Tabuada do " + numero + ":");
+        while (i <= 10) {
+            int resultado = numero * i;
+            System.out.println(numero + " x " + i + " = " + resultado);
+            i++;
+        }
+    }
+}
