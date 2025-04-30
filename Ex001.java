991d68120250942cb70188af3403052879cca8e1
JuAnselmo46 <juliana.anselmo1a@gmail.com>
2025-04-30 10:11:12 -0300

    EX001 Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].

---
+++ Ex001.java
@@ -0,0 +1,19 @@
+/***********
+Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Ex001
+{
+     public static void main(String [] args) { 
+         Scanner ler = new Scanner(System.in);
+	   int numero;
+	   
+	   System.out.println("Informe um número: ");
+	   numero = ler.nextInt();
+	   
+	   System.out.println("O número informado foi: "+numero);
+	
+	
+	}
+}
