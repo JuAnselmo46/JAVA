254d8a9c0c12801b563bc38217cd363c01f5ec62
JuAnselmo46 <juliana.anselmo1a@gmail.com>
2025-05-05 14:50:22 -0300

    Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.

---
+++ Ex017.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+ Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+
+*******************************************************************************/
+
+import java.util.Scanner;
+public class Ex017 {
+      
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+       System.out.println("Digite número pares de 1 a 50: ");
+        for (int i = 1; i <= 50; i++) {
+        if(i % 2 != 0) {
+        System.out.println(i);
+            }
+       
+        }
+    }
+}
