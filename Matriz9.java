d7a5e0307d7b967c1514ed00e85c0f1d1ffe5914
JuAnselmo46 <juliana.anselmo1a@gmail.com>
2025-05-28 11:30:39 -0300

    Desenvolva um programa que leia duas matrizes 5x2 e crie uma terceira matriz também 5x2 com o valor da soma dos elementos de mesmo índice.

---
+++ Matriz9.java
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Desenvolva um programa que leia duas matrizes 5x2 e crie uma terceira matriz também 5x2
+com o valor da soma dos elementos de mesmo índice. 
+
+*******************************************************************************/
+
+import java.util.Scanner;
+public class Matriz9
+{
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+        int [][] matriz1 = {
+                {3, 6},
+                {4, 8},
+                {2, 4},
+                {9, 18},
+                {5, 10}
+        };
+                
+         int [][] matriz2 = {
+                {1, 2},
+                {3, 4},
+                {5, 6},
+                {7, 8},
+                {9, 10}
+        };
+        int soma [][] = new int [5][2];
+        
+        System.out.println();
+        
+        for (int i = 0; i < matriz1.length; i++){
+            for (int j = 0; j < matriz1[i].length; j++){
+                soma [i][j] = matriz1 [i][j] + matriz2 [i][j];
+                System.out.println("A soma do matriz é: " + soma[i][j]);
+                }
+        }
+    }
+}
