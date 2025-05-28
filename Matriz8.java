2215350680f5a73cbf12309645c4844993bfcd78
JuAnselmo46 <juliana.anselmo1a@gmail.com>
2025-05-28 11:17:52 -0300

    Crie um algoritmo que leia uma matriz 3x3 e crie uma nova matriz que seja a matriz transposta da primeira (troque as linhas por colunas)

---
+++ Matriz8.java
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Crie um algoritmo que leia uma matriz 3x3 e crie uma nova matriz que seja a matriz
+transposta da primeira (troque as linhas por colunas)
+*******************************************************************************/
+
+import java.util.Scanner;
+public class Matriz8
+
+{
+  public static void main(String[] args) {
+        int linha =3, coluna =3;
+        int[][] matriz = {
+                {3, 2, 5},
+                {6, 4, 10},
+                {9, 6, 15}
+        };
+        
+            diss(matriz);
+            
+            int[][] transposta = new int [coluna][linha];
+            for (int i = 0; i < linha; i++){
+                 for (int j = 0; j < coluna; j++){
+                     transposta[j][i] = matriz [i][j];
+                 }
+            }
+            
+            diss(transposta);
+        }
+        
+        public static void diss(int[][] matriz) {
+            System.out.println("A matriz é: ");
+            for(int[] linha : matriz){
+                for (int coluna : linha){
+                    System.out.print(coluna + "     ");
+                }
+                System.out.println();
+            }
+        }
+    }
